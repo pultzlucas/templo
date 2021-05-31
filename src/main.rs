39197813eba2174fb15e226cc29c6796bd0cbdb3
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-05-31 16:55:00 -0300

    getting templates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct Template {
+    _id: String,
+    name: String,
+    owner: String,
+    paths: String,
+    created_at: String,
+    has_contents: bool,
+    __v: u32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("https://protternio.herokuapp.com/templates")?;
+    let templates_str = resp.text()?;
+    let templates_json: Vec<Template> = serde_json::from_str(templates_str.as_str())?;
+
+    for temp in templates_json.iter() {
+        println!("template name: {}", temp.name);
+        println!("template owner: {}", temp.owner);
+        println!("template has contents? {}\n", temp.has_contents);
+    }
+
+
+    Ok(())
 }
