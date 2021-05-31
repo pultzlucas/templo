c4a605543203ffb96762c983a6df3a0ef3b971d3
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-05-31 17:05:53 -0300

    getting args

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-extern crate serde;
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
+use std::env;
 
-#[derive(Serialize, Deserialize)]
-struct Template {
-    _id: String,
-    name: String,
-    owner: String,
-    paths: String,
-    created_at: String,
-    has_contents: bool,
-    __v: u32,
-}
+fn main() {
+    let env: Vec<String> = env::args().collect();
+    let args = &env[1..];
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://protternio.herokuapp.com/templates")?;
-    let templates_str = resp.text()?;
-    let templates_json: Vec<Template> = serde_json::from_str(templates_str.as_str())?;
-
-    for temp in templates_json.iter() {
-        println!("template name: {}", temp.name);
-        println!("template owner: {}", temp.owner);
-        println!("template has contents? {}\n", temp.has_contents);
-    }
-
-
-    Ok(())
+    println!("args: {:?}", args)
 }
