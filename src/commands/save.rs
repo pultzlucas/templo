562b862559d76ab95c1a874e35129442567f4773
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-06-02 09:24:29 -0300

    verifing if directory exists

--- src/commands/save.rs
+++ src/commands/save.rs
@@ -1,5 +1,6 @@
 use crate::core::get_template_paths;
-use crate::utils::structs::Template;
+use crate::utils::structs::HEAD;
+use std::path::Path;
 
 pub fn save(args: &[String]) -> Result<&str, String> {
     if args.len() < 1 {
@@ -9,12 +10,23 @@ pub fn save(args: &[String]) -> Result<&str, String> {
     if args.len() < 2 {
         return Err("Template name must be specified.".to_string())
     }
-
+    
     let directory = args[0].clone();
     let template_name = args[1].clone();
-    let template_head = Template {
+
+    if !Path::new(&directory).exists() {
+       return Err(format!("Error: Directory {} not exists.", directory))
+    }
+
+    let template_paths = get_template_paths(directory);
+
+    if template_paths.is_err() {
+        return Err(format!("Error: {}", template_paths.unwrap_err()))
+    }
+
+    let template_head = HEAD {
         name: template_name,
-        paths: get_template_paths(directory).join(";")
+        paths: template_paths.unwrap().join(";")
     };
 
     println!("HEAD: \n{:?}", template_head);
--- src/core/path.rs
+++ src/core/path.rs
@@ -4,13 +4,13 @@ use crate::tests;
 extern crate fs_tree;
 use fs_tree::FsTreeBuilder;
 
-pub fn get_template_paths(directory: String) -> Vec<String> {
-    let fs_tree = FsTreeBuilder::new(directory).build();
+pub fn get_template_paths(directory: String) -> Result<Vec<String>, String> {
+    let fs_tree = FsTreeBuilder::new(&directory).build();
 
-    let mut files: Vec<String> = fs_tree
+    let files: Vec<String> = fs_tree
         .iter()
         .map(|file| file.unwrap().into_os_string().into_string().unwrap())
         .collect();
 
-    files
+    Ok(files)
 }
