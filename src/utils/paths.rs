2f80898c1cb28b7d475444ae2febcb5fdaf35d07
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-06-02 18:03:57 -0300

    clearing paths file

--- src/utils/paths.rs
+++ src/utils/paths.rs
@@ -1,5 +1,1 @@
-//use std::path::{Path, PathBuf};
-
-
 pub const TEMPLATES_PATH: &'static str = "C:/Prottern/Repository/Templates";
-//pub const TEMPLATES_PATH: PathBuf = Path::new("C:/Prottern/Repository/Templates");
