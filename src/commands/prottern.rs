42b65187a119a4fa5e7935dafaff82f0a986987f
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-06-04 09:46:48 -0300

    show message if repository not exists

--- src/commands/prottern.rs
+++ src/commands/prottern.rs
@@ -1,3 +1,8 @@
+use crate::utils::paths::TEMPLATES_PATH;
+use std::path::Path;
 pub fn prottern() {
-    println!("Welcome to prottern!")
+    println!("Welcome to prottern!");
+    if !Path::new(TEMPLATES_PATH).exists() {
+        println!("Type \"prottern init\" to create a template repository\"")
+    }
 }
