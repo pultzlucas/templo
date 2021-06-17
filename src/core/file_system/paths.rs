1936d8270802ee7e3ab2d8cd9019eb8e16225511
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-06-17 20:15:54 -0300

    changing the user account auth filename

--- src/core/file_system/paths.rs
+++ src/core/file_system/paths.rs
@@ -1,2 +1,2 @@
 pub const TEMPLATES_PATH: &'static str = r"C:\Prottern\Repository\Templates";
-pub const USER_ACCOUNT_PATH: &'static str = r"C:\Prottern\current_user.json";
+pub const USER_ACCOUNT_AUTH_PATH: &'static str = r"C:\Prottern\auth.json";
--- src/core/user_account/user_account_manager.rs
+++ src/core/user_account/user_account_manager.rs
@@ -1,6 +1,6 @@
 use super::user_account_data::UserAccountData;
 use crate::core::{
-    file_system::paths::USER_ACCOUNT_PATH,
+    file_system::paths::USER_ACCOUNT_AUTH_PATH,
     requester::{Method, ProtternRequester},
 };
 use serde_derive::{Deserialize, Serialize};
@@ -48,11 +48,11 @@ impl UserAccountManager {
     pub fn log_user_account(fields: UserAccountData, key: String) -> Result<(), Error> {
         let user_account = UserAccountKey::new(fields.username, fields.email, fields.password, key);
         let content = serde_json::to_string(&user_account)?;
-        fs::write(USER_ACCOUNT_PATH, content)
+        fs::write(USER_ACCOUNT_AUTH_PATH, content)
     }
 
     pub fn get_user_account_data() -> Result<UserAccountKey, Error> {
-        let user_account = fs::read_to_string(USER_ACCOUNT_PATH).unwrap();
+        let user_account = fs::read_to_string(USER_ACCOUNT_AUTH_PATH).unwrap();
         match serde_json::from_str(&user_account) {
             Err(e) => {
                 let err = Error::new(ErrorKind::Other, e.to_string());
