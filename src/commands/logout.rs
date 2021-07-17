5f8ad37616ec3ba53a13985588e2b191f0014b67
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-07-17 15:37:49 -0300

    handling logout command exceptions

--- src/commands/logout.rs
+++ src/commands/logout.rs
@@ -1,7 +1,14 @@
 use crate::core::user_account::UserAccountManager;
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 
-pub fn logout() -> Result<(), Error>{
+pub fn logout() -> Result<(), Error> {
+    if !UserAccountManager::user_auth_exists() {
+        return Err(Error::new(
+            ErrorKind::NotFound,
+            "You already is logged out.",
+        ));
+    }
+
     UserAccountManager::logout_current()?;
     println!("Logged out.");
     Ok(())
