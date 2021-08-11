32bdae9addabd3426152eab22e007ca1dd430122
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-08-11 13:06:57 -0300

    update save command

--- src/commands/save.rs
+++ src/commands/save.rs
@@ -3,19 +3,14 @@ use crate::{
         INVALID_DIRECTORY_PATH_NAME, INVALID_TEMPLATE_NAME, NOT_FOUND_USER_AUTH,
     },
     core::{
-        file_system::ProtternFileSystem,
         repository::local,
         template::maker::create_template,
         user_account::user_auth_exists,
-        utils::errors::{invalid_input_error, not_found_error},
+        utils::errors::{already_exists_error, invalid_input_error, not_found_error},
     },
 };
 
-use std::{
-    io::{Error, ErrorKind},
-    path::Path,
-    time::Instant,
-};
+use std::{io::Error, path::Path, time::Instant};
 
 pub fn save(args: &[String]) -> Result<(), Error> {
     local::create()?;
@@ -32,30 +27,27 @@ pub fn save(args: &[String]) -> Result<(), Error> {
 
     let directory = args[0].clone();
     let template_name = args[1].clone();
-    let template_path = ProtternFileSystem::get_template_path(&template_name);
 
-    if template_path.exists() {
-        let err = Error::new(
-            ErrorKind::AlreadyExists,
-            format!(r#"Template "{}" already exists."#, &template_name),
-        );
-        return Err(err);
+    if local::template_exists(&template_name) {
+        return Err(already_exists_error(format!(
+            r#"Template "{}" already exists in your repository."#,
+            &template_name
+        )));
     }
+
     if !Path::new(&directory).exists() {
-        let err = Error::new(
-            ErrorKind::InvalidInput,
-            format!(r#"Directory "{}" not exists."#, directory),
-        );
-        return Err(err);
+        return Err(invalid_input_error(format!(
+            r#"Not is possible find "{}" directory."#,
+            directory
+        )));
     }
 
-    // Mine template from directory
     let start = Instant::now(); // start timing process
     let template = create_template(template_name, directory)?;
 
-    //save template in repository
     local::save_template(template)?;
     println!("Template was saved successfully.");
+
     let end = Instant::now(); // stop timing process
     println!("Done in {:.2?}", end.duration_since(start));
 
