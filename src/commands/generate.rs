5e4381351d08573347f554f2265f4d6cdc7fc18c
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-08-11 11:01:48 -0300

    update template manager

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,4 +1,4 @@
-use crate::core::template::TemplateManager;
+use crate::core::template::gen_template;
 use crate::core::utils::errors::invalid_input_error;
 use crate::{
     cli::output::messages::error::{
@@ -18,23 +18,24 @@ pub fn generate(args: &[String]) -> Result<(), Error> {
     if args.len() < 2 {
         return Err(invalid_input_error(INVALID_DIRECTORY_PATH_NAME));
     }
+
     let template_name = &args[0];
     let directory = Path::new(&args[1]);
+
     if directory.extension() != None {
         return Err(invalid_input_error(INVALID_DIRECTORY_PATH_TYPE));
     }
+
     if !directory.exists() {
         fs::create_dir_all(directory)?;
     }
 
-    // Get template from repository
     let start = Instant::now(); // start timing process
     let template = local::get_template(&template_name)?;
-    let manager = TemplateManager::new(vec![template]);
 
-    // Generate template
-    manager.gen_templates(directory)?;
+    gen_template(template, directory)?;
     println!("Template \"{}\" was generated.", template_name);
+
     let end = Instant::now(); // stop timing process
     println!("Done in {:.2?}", end.duration_since(start));
     Ok(())
--- src/core/template/manager.rs
+++ src/core/template/manager.rs
@@ -1,45 +1,61 @@
 use super::Template;
+use crate::core::template::miner::File;
 use crate::{core::utils::path::pathbuf_to_string, paint_string};
-use std::{fs, io::Error, path::Path};
+use std::{
+    fs,
+    io::Error,
+    path::{Path, PathBuf},
+};
 
-pub struct TemplateManager {
-    templates: Vec<Template>,
+pub fn gen_template(template: Template, directory: &Path) -> Result<(), Error> {
+    for path in template.paths.into_iter() {
+        create_path(path, directory)?;
+    }
+
+    if template.contents.len() > 0 {
+        write_contents(template.contents, directory)?;
+    }
+    Ok(())
 }
 
-impl TemplateManager {
-    pub fn new(templates: Vec<Template>) -> Self {
-        Self { templates }
+fn create_path(path: PathBuf, directory: &Path) -> Result<(), Error> {
+    let real_path = get_real_path(directory, path);
+
+    if real_path.is_file() {
+        create_file(&real_path)?;
+    }
+    if real_path.is_dir() {
+        create_dir(&real_path)?;
     }
 
-    pub fn gen_templates(&self, directory: &Path) -> Result<(), Error> {
-        for template in self.templates.clone().into_iter() {
-            // creating files and directories
-            for path in template.paths.into_iter() {
-                let real_path = Path::new(directory)
-                    .join(pathbuf_to_string(path))
-                    .to_path_buf();
-                if real_path.is_file() {
-                    fs::write(&real_path, "")?;
-                    println!("{} {:?}", paint_string!("{gray}", "file:"), real_path);
-                }
+    Ok(())
+}
 
-                if real_path.is_dir() {
-                    fs::create_dir(&real_path)?;
-                    println!(" {} {:?}", paint_string!("{gray}", "dir:"), real_path);
-                }
-            }
-
-            if template.contents.len() > 0 {
-                // writing the files content
-                for data in template.contents.into_iter() {
-                    let file_path = Path::new(directory).join(data.filename);
-                    if file_path.exists() {
-                        fs::write(file_path, data.content)?;
-                    }
-                }
-            };
+fn write_contents(contents: Vec<File>, directory: &Path) -> Result<(), Error> {
+    for data in contents.into_iter() {
+        let file_path = Path::new(directory).join(data.filename);
+        if file_path.exists() {
+            fs::write(file_path, data.content)?;
         }
-
-        Ok(())
     }
+
+    Ok(())
+}
+
+fn get_real_path(directory: &Path, path: PathBuf) -> PathBuf {
+    Path::new(directory)
+        .join(pathbuf_to_string(path))
+        .to_path_buf()
+}
+
+fn create_file(path: &PathBuf) -> Result<(), Error> {
+    fs::write(path, "")?;
+    println!("{} {:?}", paint_string!("{gray}", "file:"), path);
+    Ok(())
+}
+
+fn create_dir(path: &PathBuf) -> Result<(), Error> {
+    fs::create_dir(path)?;
+    println!(" {} {:?}", paint_string!("{gray}", "dir:"), path);
+    Ok(())
 }
