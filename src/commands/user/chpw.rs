215dbcbdf9e5917c4a28fa3e71da65640ef48cde
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-08-31 20:52:25 -0300

    request change password

--- src/commands/user/chpw.rs
+++ src/commands/user/chpw.rs
@@ -1,6 +1,21 @@
 use std::io::Error;
+use crate::core::requester::{build_request, request, Method, AUTHENTICATOR_URL};
+use crate::core::user_account::get_user_account_data;
+use crate::utils::errors::std_error;
+use serde_json::to_string;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct ChangePassRequest {
+    username: String
+}
 
 pub async fn run() -> Result<(), Error> {
-    
+    let username = get_user_account_data()?.username;
+    let url = format!("{}/user/changePassword", AUTHENTICATOR_URL);
+    let body = ChangePassRequest {username};
+    let req = build_request(&url, Method::POST, std_error(to_string(&body))?);
+    let res = request(req).await?;
+    println!("{}", res);
     Ok(())
 }
--- src/core/requester/mod.rs
+++ src/core/requester/mod.rs
@@ -3,3 +3,5 @@ mod requester;
 pub use hyper::http::HeaderValue;
 pub use hyper::Method;
 pub use requester::{build_request, request};
+
+pub const AUTHENTICATOR_URL: &'static str = "https://prottern-authenticator.herokuapp.com";
