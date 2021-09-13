bd8db29f00029a12a245034d7364dca3988940cb
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-09-13 18:58:22 -0300

    update update method

--- src/cli/output/messages.rs
+++ src/cli/output/messages.rs
@@ -1,6 +1,5 @@
 pub mod error {
     pub const INVALID_TEMPLATE_NAME: &'static str = "Template name must be specified.";
-    pub const INVALID_DIRECTORY_PATH_NAME: &'static str = "Directory path must be specified.";
     pub const INVALID_DIRECTORY_PATH_TYPE: &'static str = "The path should be a directory.";
     pub const TEMPLATE_ALREADY_EXISTS: &'static str = "Template already exists on repository.";
 }
--- src/methods/update.rs
+++ src/methods/update.rs
@@ -1,13 +1,9 @@
 use crate::cli::input::args::Args;
-use crate::cli::output::messages::error::{
-    INVALID_DIRECTORY_PATH_NAME, INVALID_DIRECTORY_PATH_TYPE, INVALID_TEMPLATE_NAME,
-};
+use crate::cli::output::messages::error::INVALID_TEMPLATE_NAME;
 use crate::core::repo;
 use crate::core::template::maker::make_template;
 use crate::methods::check_flags;
 use crate::utils::errors::invalid_input_error;
-use crate::utils::path::str_to_pathbuf;
-use std::fs;
 use std::io::Error;
 use std::time::Instant;
 
@@ -44,24 +40,8 @@ pub fn run(args: Args) -> Result<(), Error> {
         return Err(invalid_input_error(INVALID_TEMPLATE_NAME));
     }
 
-    if args.inputs.len() < 2 {
-        return Err(invalid_input_error(INVALID_DIRECTORY_PATH_NAME));
-    }
-
-    let template_name = args.inputs[0].clone();
-    let directory = &args.inputs[1];
-    let directory_pathbuf = str_to_pathbuf(directory);
-
-    if directory_pathbuf.extension() != None {
-        return Err(invalid_input_error(INVALID_DIRECTORY_PATH_TYPE));
-    }
-
-    if !directory_pathbuf.exists() {
-        fs::create_dir_all(directory_pathbuf)?;
-    }
-
     let start = Instant::now(); // start timing process
-
+    let template_name = args.inputs[0].clone();
     let new_template = make_template(template_name.clone())?;
     repo::update_template_content(template_name.clone(), new_template)?;
 
