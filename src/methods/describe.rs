1ffc8748ab1b69a16fdcf352aec58952cafe52bc
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-10-04 20:56:01 -0300

    update describe method

--- src/methods/describe.rs
+++ src/methods/describe.rs
@@ -1,6 +1,8 @@
 use crate::cli::input::command::Command;
 use crate::cli::output::messages::error::INVALID_TEMPLATE_NAME;
+use crate::core::namespaces::{get_repo_namespace_obj, NamespaceObject};
 use crate::core::repos::Repository;
+use crate::paintln;
 use crate::utils::errors::invalid_input_error;
 use crate::utils::path::pathbuf_to_string;
 use std::io::Error;
@@ -11,10 +13,22 @@ pub fn run(command: Command) -> Result<(), Error> {
     }
 
     // Get template from repository
-    let repo = Repository::connect(command.args[1].clone())?;
-    let template = repo.get_template(&command.args[0])?;
+    let NamespaceObject {
+        repo_name,
+        template_name,
+    } = get_repo_namespace_obj(&command.args[0]);
+    let repo = Repository::connect(repo_name)?;
+    let template = repo.get_template(&template_name)?;
 
-    // Describe template
+    paintln!("> {yellow}", template.name);
+
+    // Display template description
+    if let Some(description) = template.description {
+        println!("{}", description);
+    }
+    
+    // Display template paths
+    paintln!("{gray}", "[paths]");
     template
         .paths
         .into_iter()
