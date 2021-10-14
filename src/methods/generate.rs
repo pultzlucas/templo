cff0ae2e3b38472b51cbd8f430221d7e7bdd5089
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-10-14 19:58:30 -0300

    update gen and get method

--- src/methods/generate.rs
+++ src/methods/generate.rs
@@ -1,16 +1,12 @@
-use serde_json::from_str;
-
-use crate::cli::input::check_flags;
 use crate::cli::input::command::{Command, CommandOption};
 use crate::cli::input::namespaces::get_repo_namespace_obj;
 use crate::cli::output::messages::error::{INVALID_DIRECTORY_PATH_TYPE, INVALID_TEMPLATE_NAME};
-use crate::core::http::{str_is_url, validate_url};
 use crate::core::repos::Repository;
 use crate::core::template::engine::{get_engine_args_input, set_arg_default_value, TempEngineArg};
-use crate::core::template::getter::get_remote_template;
 use crate::core::template::{generator, Template};
 use crate::core::utils::errors::{invalid_input_error, std_error};
-use crate::{paintln, write_help};
+use crate::write_help;
+use serde_json::from_str;
 use std::{fs, io::Error, path::Path, time::Instant};
 
 pub struct Generate;
@@ -26,15 +22,8 @@ impl Generate {
             return Ok(());
         }
 
-        let flags = vec!["--file", "-f", "--remote"];
-        check_flags(&command.flags, flags)?;
-
-        if command.has_flag("-f") || command.has_flag("--file") {
+        if command.has_option("file") {
             return gen_from_template_file(command.clone());
-        }
-
-        if command.has_flag("--remote") {
-            return gen_from_remote_template(command.clone()).await;
         }
 
         if command.args.len() < 1 {
@@ -78,14 +67,15 @@ impl Generate {
 fn gen_from_template_file(command: Command) -> Result<(), Error> {
     let start = Instant::now();
 
-    let tpo_filename = &command.args[0];
+    let tpo_filename = &command.get_opt_by_name("file").unwrap().value;
+
     let directory = if command.args.len() < 2 {
         Path::new(".")
     } else {
         Path::new(&command.args[1])
     };
 
-    if directory.extension() != None {
+    if directory.extension().is_some() {
         return Err(invalid_input_error(INVALID_DIRECTORY_PATH_TYPE));
     }
 
@@ -108,58 +98,6 @@ fn gen_from_template_file(command: Command) -> Result<(), Error> {
     let end = Instant::now(); // stop timing process
     println!("Done in {:.2?}", end.duration_since(start));
     Ok(())
-}
-
-async fn gen_from_remote_template(command: Command) -> Result<(), Error> {
-    let start = Instant::now();
-
-    if command.args.len() < 1 {
-        return Err(invalid_input_error("The template url must be specified."));
-    }
-
-    let directory = if command.args.len() < 2 {
-        Path::new(".")
-    } else {
-        Path::new(&command.args[1])
-    };
-
-    if directory.extension() != None {
-        return Err(invalid_input_error(INVALID_DIRECTORY_PATH_TYPE));
-    }
-    
-    if !str_is_url(&command.args[0]) {
-        return Err(invalid_input_error("The template url must be valid."));
-    }
-
-    let url =  &command.args[0];
-
-    validate_url(url)?;
-    if url.is_empty() {
-        return Err(invalid_input_error("The template url must be specified."));
-    }
-
-    let key = if command.has_option("auth") {
-        let key = command.get_opt_by_name("auth").unwrap();
-        Some(key.value.clone())
-    } else {
-        None
-    };
-
-    paintln!("{gray}", "[getting template]");
-    let template = get_remote_template(&url, key).await?.template;
-
-    let temp_args = if !command.options.is_empty() {
-        get_template_args_by_options(command.options.clone(), &template)?
-    } else {
-        get_template_args_by_temp(&template)?
-    };
-
-    generator::gen_template(template.clone(), directory, temp_args)?;
-    println!("Template \"{}\" was generated.", template.name);
-
-    let end = Instant::now(); // stop timing process
-    println!("Done in {:.2?}", end.duration_since(start));
-    return Ok(());
 }
 
 fn get_template_args_by_options(
